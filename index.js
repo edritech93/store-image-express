d3b9f5d6b0ebcdf365b8d251f7e74c8029dd5911
Yudi Alviska <yudi.a@weefer.co.id>
2023-09-04 14:47:50 +0700

    edit .gitignore

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
+app.use("/attachments", express.static(path.join(__dirname, "attachments")));
 app.get("/", (req, res) => {
-  res.send("Hello, this is Express + TypeScript");
+  res.send("Hello, this is ExpressJS");
 });
 
 app.listen(port, () => {
