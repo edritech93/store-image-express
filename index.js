0f2532bc8227fc50d08dffcbc9f547e9f32f6da0
Yudi Alviska <yudi.a@weefer.co.id>
2023-09-04 16:35:15 +0700

    add route import-list

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
 
-app.use("/attachments", express.static(path.join(__dirname, "attachments")));
+const pathAttachments = path.join(__dirname, "attachments");
+
+app.use("/attachments", express.static(pathAttachments));
 app.get("/", (req, res) => {
   res.send("Hello, this is ExpressJS");
+});
+app.get("/import-list", async (req, res) => {
+  const dataPath = await getAllPath();
+  const arrayRes = dataPath.map((e) => {
+    return `/attachments/${e}`;
+  });
+  res.status(200).json(arrayRes);
 });
 
 app.listen(port, () => {
   console.log(`[Server]: I am running at http://localhost:${port}`);
 });
+
+function getAllPath() {
+  return new Promise((resolve, reject) => {
+    fs.readdir(pathAttachments, function (error, files) {
+      if (error) {
+        reject(error);
+        return;
+      }
+      const targetFiles = files.filter((file) => {
+        return path.extname(file).toLowerCase() === ".jpg";
+      });
+      resolve(targetFiles);
+    });
+  });
+}
